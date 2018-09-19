6427fc35055c2c576794cf361321bc64f24851e1
Pierre Wielders <pierre@wielders.net>
2018-09-19 20:39:43 +0200

    [IDRM] Extend the OpenCDM(i) nterface towards the DRM on decrypt level with a KeyId.

--- MediaSession.cpp
+++ MediaSession.cpp
@@ -268,7 +268,10 @@ CDMi_RESULT MediaKeySession::Decrypt(
     const uint8_t *f_pbData,
     uint32_t f_cbData,
     uint32_t *f_pcbOpaqueClearContent,
-    uint8_t **f_ppbOpaqueClearContent) {
+    uint8_t **f_ppbOpaqueClearContent,
+    const uint8_t keyIdLength,
+    const uint8_t* keyId)
+{
   widevine::Cdm::KeyStatusMap map;
   std::string keyStatus;
 
--- MediaSession.h
+++ MediaSession.h
@@ -65,7 +65,10 @@ public:
         const uint8_t *f_pbData,
         uint32_t f_cbData,
         uint32_t *f_pcbOpaqueClearContent,
-        uint8_t **f_ppbOpaqueClearContent);
+        uint8_t **f_ppbOpaqueClearContent,
+        const uint8_t keyIdLength,
+        const uint8_t* keyId);
+
 
     virtual CDMi_RESULT ReleaseClearContent(
         const uint8_t *f_pbSessionKey,
