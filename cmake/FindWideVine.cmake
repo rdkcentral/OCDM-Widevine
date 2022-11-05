064afe985d65549a197d8fd295235813d50beb22
Pierre Wielders <pierre@wielders.net>
2022-11-05 13:41:51 +0100

    [BUILD] Use the new clientlibrraies.

--- cmake/FindWideVine.cmake
+++ cmake/FindWideVine.cmake
@@ -63,8 +63,8 @@ find_package(WPEFrameworkCore REQUIRED)
 find_package(CompileSettingsDebug REQUIRED)
 list(APPEND WIDEVINE_LIBRARIES ${NAMESPACE}Core::${NAMESPACE}Core)
 
-find_package(deviceinfo REQUIRED)
-list(APPEND WIDEVINE_LIBRARIES deviceinfo::deviceinfo)
+find_package(${NAMESPACE}DeviceInfo REQUIRED)
+list(APPEND WIDEVINE_LIBRARIES ${NAMESPACE}DeviceInfo::${NAMESPACE}DeviceInfo)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Widevine DEFAULT_MSG WIDEVINE_INCLUDE_DIRS WIDEVINE_LIBRARIES)
